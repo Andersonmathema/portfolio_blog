721b56586afeea2450115b3fcd9fb5c02809b542
Anderson Andrade Silva <ander.math89@gmail.com>
2025-02-05 23:29:47 -0300

    Implementação: perfil2

--- app.py
+++ app.py
@@ -137,17 +137,18 @@ def public_page():
     st.title("Portfólio Desenvolvimento de sistemas")
 
     col1, col2 = st.columns(2)
-    with col2:
-        st.text("""Sou Ana Lívia Nunes de Carvalho, professora da rede estadual de ensino de São Paulo,
-                 atualmente, trabalho na Escola Estadual Maria Carolina de Lima de Ensino Integral,
-                 localizada em Nuporanga. Nessa escola atuo como professora de Língua Portuguesa
-                 e Redação e Leitura.
+    with col1:
+        st.text("""Sou Anderson Andrade Silva, professor da rede estadual de ensino de São Paulo,
+                 atualmente, trabalho na Escola Estadual Amador e Catharina Saporito Augusto de Ensino Integral,
+                 localizada em São Paulo. Nessa escola atuo como professor de Desenvolvimento de Software.
                 Este portifólio tem intuito de evidenciar ações desenvolvidas na escola, além de servir
                 como replicabilidade do Programa Ensino Integral.
-                Formação acadêmica: Licenciatura em Letras- Português e Inglês pela Barão de Mauá
-                Pós em História e Cultura Afro-Brasileiras., pela Unyleya.""")
-        
-        st.image("uploads/foto3-2025.jpg")
+                Formação acadêmica: Licenciatura em Matemática pela UNIBAN, Bacharel em Ciências da computação
+                pelo Centro Universitário FAM e Pós em Robótica Educacional Criativa pelo SESI-SP.""")
+    with col2:    
+        image = Image.open("uploads/foto3-2025.jpg")
+        image.thumbnail((600, 400))
+        st.image(image)
 
     db = SessionLocal()
     posts = db.query(Post).all()
